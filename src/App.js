0ef23a1fcf3d29ec5e1e06ae395bf8047135495f
Sageyuva <yuvarajdhammure@gmail.com>
2023-10-06 14:52:37 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,17 +1,70 @@
-import React from 'react';
+import React ,{ useState} from 'react';
+import "./Style.css"
+import { Plus ,Trash2 } from 'lucide-react';
+
 
 function App() {
+
+
+const [InputText, setInputText] = useState("")
+const [ItemData, setItemData] = useState([])
+
+
+const OnUpdate = (e) => {
+  setInputText(e.target.value);
+}
+
+const AddData = () => {
+  if(!InputText)(
+    alert("Cannot Add empty space")
+  )
+else{
+ setItemData([ ...ItemData ,InputText])
+ setInputText("")
+}
+}
+
+const DelTem = (id) => {
+
+   const UpdatedData = ItemData.filter((elem, ind)=>{
+    return ind != id;
+   })
+   setItemData(UpdatedData)
+   
+}
+
+
   return (
-    <div style={{ textAlign: 'center' }}>
-      <header>
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a href="https://reactjs.org" target="_blank" rel="noopener noreferrer">
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+    <center>
+      <h1 className='too'> ToDo</h1>
+    </center>
+      <center>
+        <div className="maindiv">
+         <div  className='inputarea' > 
+         <textarea onChange={OnUpdate} value={InputText} className='textin' type="text" />
+         <button onClick={AddData} className="addB" title='Add Item'>
+         <Plus/>
+         </button>
+         </div>
+         
+           {
+            ItemData.map((elem , ind)=> {
+             return(
+              <div className='itemsDiv' key={ind}>
+              <p className='ItemName'>
+            {elem}</p> 
+            <button className='Del' title='Delete Item' onClick={()=> DelTem(ind)}><Trash2/></button>
+              </div>
+             )
+            })
+           }
+         
+
+        </div>
+       
+      </center>
+    </>
   );
 }
 
